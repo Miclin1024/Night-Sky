fe908bcb26c53c342f9cf1705c9a4fd4435533c8
Michael Lin <miclin@berkeley.edu>
2020-03-05 01:00:39 -0800

    Initial Commit

---
+++ Dark Sky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dark Sky
+//
+//  Created by Michael Lin on 3/5/20.
+//  Copyright © 2020 Michael Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
